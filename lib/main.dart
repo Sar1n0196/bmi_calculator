9e2d8855b9d051b05735160748de32051cc87af5
Sar1n0196 <sushrutkulkarni0196@gmail.com>
2022-08-07 00:00:47 +0530

    changes to main

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class _BMICalState extends State<BMICal> {
   Widget build(BuildContext context) {
     return Container(
         child: Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Color.fromARGB(255, 16, 16, 16),
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: GradientText(
@@ -44,8 +44,9 @@ class _BMICalState extends State<BMICal> {
           style: const TextStyle(
             fontSize: 50.0,
           ),
-          gradientType: GradientType.linear,
-          gradientDirection: GradientDirection.ltr,
+          gradientType: GradientType.radial,
+          gradientDirection: GradientDirection.ttb,
+          radius: 5,
           colors: [
             const Color.fromARGB(255, 255, 0, 140),
             const Color.fromARGB(255, 85, 17, 181),
@@ -55,19 +56,94 @@ class _BMICalState extends State<BMICal> {
         elevation: 2.0,
       ),
       body: SingleChildScrollView(
-        child: Padding(padding: EdgeInsets.all(15),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                radioButton("Male", Colors.blueAccent, 1),
-                radioButton("Female", Colors.pinkAccent, 0)
-              ],
-            )
-          ],
-        ),
-        
-        
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  radioButton("Male", Colors.blueAccent, 1),
+                  radioButton("Female", Colors.pinkAccent, 0)
+                ],
+              ),
+              SizedBox(
+                height: 23,
+              ),
+              Text(
+                "Height",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 152, 152, 152), fontSize: 21),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              TextField(                
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    hintText: "Your Height in cm",
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 30, 30, 30),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(13),
+                        borderSide: BorderSide.none)),
+              ),
+              SizedBox(
+                height: 23,
+              ),
+              Text(
+                "Weight",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 152, 152, 152), fontSize: 21),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    hintText: "Your Weight in kg",
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 30, 30, 30),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(13),
+                        borderSide: BorderSide.none)),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: double.infinity,
+                child: FlatButton(
+                  height: 50,
+                  onPressed: () {},
+                  color: Color.fromARGB(255, 187, 134, 252),                  
+                  child: Text(
+                    "Calculate",
+                    style: TextStyle(
+                      fontSize: 19,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 30, 30, 30),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              Text(
+                "Your BMI is: ",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 152, 152, 152)),
+              )
+            ],
+          ),
         ),
       ),
     ));
@@ -83,16 +159,22 @@ class _BMICalState extends State<BMICal> {
     return Expanded(
       child: Container(
         margin: EdgeInsets.symmetric(horizontal: 15),
+        height: 80,
         child: FlatButton(
-            color: currentindex == index ? color : Colors.grey,
+            color:
+                currentindex == index ? color : Color.fromARGB(255, 30, 30, 30),
             onPressed: () {
               changeIndex(index);
             },
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(7)),
-            child: Text(value, style: TextStyle(
-              color: currentindex == index ? Colors.white : color,
-            ),)),
+            child: Text(
+              value,
+              style: TextStyle(
+                  color: currentindex == index ? Colors.white : color,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            )),
       ),
     );
   }
